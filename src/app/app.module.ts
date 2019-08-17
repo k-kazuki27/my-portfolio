71501d30db71f40931229d37bc5f03d7976e568a
kazuki.kawabata <>
2019-08-17 21:02:28 +0900

    add top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, TopComponent],
   imports: [BrowserModule, ScrollToModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
