5e497c958d3c81f0327757a18a46599783f2b4dd
kazuki.kawabata <>
2019-08-18 13:36:30 +0900

    modify works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopComponent } from './top/top.component';
+import { WorksComponent } from './works/works.component';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TopComponent],
-  imports: [BrowserModule, ScrollToModule.forRoot()],
+  declarations: [AppComponent, HeaderComponent, TopComponent, WorksComponent],
+  imports: [BrowserModule, ScrollToModule.forRoot(), LazyLoadImageModule],
   providers: [],
   bootstrap: [AppComponent]
 })
